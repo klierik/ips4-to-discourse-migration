e5e6b68bf3b5a3ec8e7933fa54d0b1932d74f00a
Oleksii Filippovych <alex.filippovich@gmail.com>
2026-03-24 19:33:25 +0300

    feat: Add GNU General Public License (GPLv3) header to all IPBoard4 migration scripts.

--- ipboard4_import_profiles.rb
+++ ipboard4_import_profiles.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
+# IPS4 to Discourse Migration Toolkit
+# Copyright (C) 2026 Oleksii Filippovych
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # =============================================================================
 # ipboard4_import_profiles.rb
 #
